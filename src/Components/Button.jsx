d1a59d0cd88dfab7d16688d89d20d8d0098f11c3
Akash Nagaonkar <akash.nagaonkar@infosys.com>
2023-09-11 19:35:51 +0530

    Completed

---
+++ src/Components/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor} hover:bg-black hover:text-white hover:scale-105`
+          : "bg-coral-red text-white border-coral-red hover:scale-105 hover:bg-red-600"
+      } rounded-full ${fullWidth && "w-full"}"}`}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
